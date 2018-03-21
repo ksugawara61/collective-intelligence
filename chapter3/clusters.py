c766c97f7d4b26883340174ded18591deb392fef
ksugawara61 <katsuya@kokohadoko.net>
2018-03-21 18:00:31 +0900

    [add] デンドログラムをJPGに書き出す処理を実装

--- chapter3/clusters.py
+++ chapter3/clusters.py
@@ -1,3 +1,5 @@
+from PIL import Image,ImageDraw
+
 def readfile(filename):
     lines = [line for line in open(filename)]
 
@@ -102,3 +104,56 @@ def printclust(clust, labels = None, n = 0):
         printclust(clust.left,  labels = labels, n = n + 1)
     if clust.right != None:
         printclust(clust.right, labels = labels, n = n + 1)
+
+
+def getheight(clust):
+    # 終端であれば高さは1にする
+    if clust.left == None and clust.right == None:
+        return 1
+
+    # そうでなければ高さはそれぞれの枝の高さの合計
+    return getheight(clust.left) + getheight(clust.right)
+
+def getdepth(clust):
+    # 終端への距離は0.0
+    if clust.left == None and clust.right == None:
+        return 0
+
+    # 枝の距離は２つの方向の大きい方にそれ自身の距離を足したもの
+    return max(getdepth(clust.left), getdepth(clust.right)) + clust.distance
+
+def drawdendrogram(clust, labels, jpeg = 'clusters.jpg'):
+    h = getheight(clust) * 20
+    w = 1200
+    depth = getdepth(clust)
+
+    scaling = float(w - 150) / depth
+
+    img = Image.new('RGB', (w,h), (255, 255, 255))
+    draw = ImageDraw.Draw(img)
+    draw.line((0, h / 2, 10, h / 2), fill = (255, 0, 0))
+
+    drawnode(draw, clust, 10, (h / 2), scaling, labels)
+    img.save(jpeg, 'JPEG')
+
+
+def drawnode(draw, clust, x, y, scaling, labels):
+    if clust.id < 0:
+        h1 = getheight(clust.left) * 20
+        h2 = getheight(clust.right) * 20
+        top = y - (h1 + h2) / 2
+        bottom = y + (h1 + h2) / 2
+
+        ll = clust.distance * scaling
+
+        draw.line((x, top + h1 / 2, x, bottom - h2 / 2), fill = (255, 0, 0))
+
+        draw.line((x, top + h1 / 2, x + ll, top + h1 / 2), fill = (255, 0, 0))
+
+        draw.line((x, bottom - h2 / 2, x + 11, bottom - h2 / 2), fill = (255, 0, 0))
+
+        drawnode(draw, clust.left, x + ll, top + h1 / 2, scaling, labels)
+        drawnode(draw, clust.right, x + ll, bottom + h2 / 2, scaling, labels)
+
+    else:
+        draw.text((x + 5, y - 7), labels[clust.id], (0,0,0))
